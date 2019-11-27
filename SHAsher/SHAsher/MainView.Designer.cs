71f4a68911534ae8593d30b900d0ace715cff5ce
Dominik Wolny <wolny.dominik@outlook.com>
2019-11-27 13:34:16 +0100

    Created main application project with simple UI
    included UI is just simple 'hello world'-ish thing. It just copies stuff from one txtbox to another.

---
+++ SHAsher/SHAsher/MainView.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace SHAsher
+{
+    partial class MainView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTxt = new System.Windows.Forms.TextBox();
+            this.outputTxt = new System.Windows.Forms.TextBox();
+            this.hashBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inputTxt
+            // 
+            this.inputTxt.Location = new System.Drawing.Point(12, 12);
+            this.inputTxt.Name = "inputTxt";
+            this.inputTxt.Size = new System.Drawing.Size(776, 20);
+            this.inputTxt.TabIndex = 0;
+            // 
+            // outputTxt
+            // 
+            this.outputTxt.Location = new System.Drawing.Point(12, 181);
+            this.outputTxt.Name = "outputTxt";
+            this.outputTxt.Size = new System.Drawing.Size(776, 20);
+            this.outputTxt.TabIndex = 1;
+            // 
+            // hashBtn
+            // 
+            this.hashBtn.Location = new System.Drawing.Point(712, 59);
+            this.hashBtn.Name = "hashBtn";
+            this.hashBtn.Size = new System.Drawing.Size(75, 23);
+            this.hashBtn.TabIndex = 3;
+            this.hashBtn.Text = "Hash!";
+            this.hashBtn.UseVisualStyleBackColor = true;
+            this.hashBtn.Click += new System.EventHandler(this.hashBtn_Click);
+            // 
+            // MainView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.hashBtn);
+            this.Controls.Add(this.outputTxt);
+            this.Controls.Add(this.inputTxt);
+            this.Name = "MainView";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTxt;
+        private System.Windows.Forms.TextBox outputTxt;
+        private System.Windows.Forms.Button hashBtn;
+    }
+}
+
---
+++ SHAsher/SHAsher/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SHAsher")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SHAsher")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fa1f2e14-e049-4568-842e-32ac16f3698a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
