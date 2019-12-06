7d4508aaa4599e416e30be89d623125e0bed7f3f
Rzohfa <wolny.dominik@outlook.com>
2019-12-06 15:41:13 +0100

    Finalized and exported High Level Library
    It's just first version of this DLL though.

--- HighLevelHashLibrary/HighLevelHashLibrary/HighLvlHasher.h
+++ HighLevelHashLibrary/HighLevelHashLibrary/HighLvlHasher.h
@@ -1,9 +1,22 @@
-#pragma once
+#ifndef HIGHLVLHASHER_H
+#define HIGHLVLHASHER_H
 
-#ifdef HIGHLVLHASHER_EXPORTS
-#define HIGHLVLHASHER_API __declspec(dllexport)
-#else
-#define HIGHLVLHASHER_API __declspec(dllimport)
+#include <vector>
+#include <cstdint>
+
+namespace hasher
+{
+	extern "C" _declspec(dllexport) void _cdecl hash(unsigned char* bytes, int Nblocks, char* return_buffer);
+
+	uint32_t rotate_right(uint32_t input, uint32_t bit_count);
+	uint32_t Ch(uint32_t x, uint32_t y, uint32_t z);
+	uint32_t Maj(uint32_t x, uint32_t y, uint32_t z);
+	uint32_t big_sigma0(uint32_t x);
+	uint32_t big_sigma1(uint32_t x);
+	uint32_t small_sigma0(uint32_t x);
+	uint32_t small_sigma1(uint32_t x);
+	void init_hash_vals();
+	void calculate_hash();
+}
+
 #endif
-
-extern "C" HIGHLVLHASHER_API void hash();
--- HighLevelHashLibrary/HighLevelHashLibrary/HighLvlHasher_internal.h
+++ HighLevelHashLibrary/HighLevelHashLibrary/HighLvlHasher_internal.h
@@ -2,16 +2,11 @@
 #define HIGHLVLHASHER_INTERNAL
 
 #include <cstdint>
+#include <vector>
 
-uint32_t rotate_right (uint32_t input, uint32_t bit_count);
-uint32_t Ch (uint32_t x, uint32_t y, uint32_t z);
-uint32_t Maj (uint32_t x, uint32_t y, uint32_t z);
-uint32_t big_sigma0 (uint32_t x);
-uint32_t big_sigma1 (uint32_t x);
-uint32_t small_sigma0 (uint32_t x);
-uint32_t small_sigma1 (uint32_t x);
-void init_hash_vals();
-void calculate_hash();
-void output_hash();
+namespace hasher
+{
+	
+}
 
 #endif // !HIGHLVLHASHER_INTERNAL
