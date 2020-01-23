ac530ac625ed89abc7b061a8ff0b16678a580b30
Dominik Wolny <wolny.dominik@outlook.com>
2020-01-23 22:58:13 +0100

    99.9% of GUI done

--- SHAsher/SHAsher/MainView.Designer.cs
+++ SHAsher/SHAsher/MainView.Designer.cs
@@ -28,28 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.inputTxt = new System.Windows.Forms.TextBox();
-            this.outputTxt = new System.Windows.Forms.TextBox();
             this.hashBtn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dllChooseCB = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.threadCounter = new System.Windows.Forms.NumericUpDown();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.inFileBrowseBtn = new System.Windows.Forms.Button();
+            this.inFileBrowseTxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.outFileBrowseBtn = new System.Windows.Forms.Button();
+            this.outFileBrowseTxt = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.threadCounter)).BeginInit();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // inputTxt
-            // 
-            this.inputTxt.Location = new System.Drawing.Point(12, 12);
-            this.inputTxt.Name = "inputTxt";
-            this.inputTxt.Size = new System.Drawing.Size(776, 20);
-            this.inputTxt.TabIndex = 0;
-            // 
-            // outputTxt
-            // 
-            this.outputTxt.Location = new System.Drawing.Point(12, 181);
-            this.outputTxt.Name = "outputTxt";
-            this.outputTxt.Size = new System.Drawing.Size(776, 20);
-            this.outputTxt.TabIndex = 1;
             // 
             // hashBtn
             // 
-            this.hashBtn.Location = new System.Drawing.Point(712, 59);
+            this.hashBtn.Location = new System.Drawing.Point(336, 144);
             this.hashBtn.Name = "hashBtn";
             this.hashBtn.Size = new System.Drawing.Size(75, 23);
             this.hashBtn.TabIndex = 3;
@@ -57,26 +58,183 @@
             this.hashBtn.UseVisualStyleBackColor = true;
             this.hashBtn.Click += new System.EventHandler(this.hashBtn_Click);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dllChooseCB);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.threadCounter);
+            this.groupBox1.Location = new System.Drawing.Point(12, 99);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(208, 75);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Properties";
+            // 
+            // dllChooseCB
+            // 
+            this.dllChooseCB.FormattingEnabled = true;
+            this.dllChooseCB.Items.AddRange(new object[] {
+            "C++",
+            "Assembler"});
+            this.dllChooseCB.Location = new System.Drawing.Point(100, 47);
+            this.dllChooseCB.Name = "dllChooseCB";
+            this.dllChooseCB.Size = new System.Drawing.Size(102, 21);
+            this.dllChooseCB.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 50);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "C++/Assembler:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Liczba wątków";
+            // 
+            // threadCounter
+            // 
+            this.threadCounter.Location = new System.Drawing.Point(100, 21);
+            this.threadCounter.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.threadCounter.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.threadCounter.Name = "threadCounter";
+            this.threadCounter.Size = new System.Drawing.Size(47, 20);
+            this.threadCounter.TabIndex = 0;
+            this.threadCounter.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.inFileBrowseBtn);
+            this.groupBox2.Controls.Add(this.inFileBrowseTxt);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.outFileBrowseBtn);
+            this.groupBox2.Controls.Add(this.outFileBrowseTxt);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(405, 81);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Input/Output";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Input file";
+            // 
+            // inFileBrowseBtn
+            // 
+            this.inFileBrowseBtn.Location = new System.Drawing.Point(324, 18);
+            this.inFileBrowseBtn.Name = "inFileBrowseBtn";
+            this.inFileBrowseBtn.Size = new System.Drawing.Size(75, 21);
+            this.inFileBrowseBtn.TabIndex = 10;
+            this.inFileBrowseBtn.Text = "Browse...";
+            this.inFileBrowseBtn.UseVisualStyleBackColor = true;
+            this.inFileBrowseBtn.Click += new System.EventHandler(this.InFileBrowseBtn_Click);
+            // 
+            // inFileBrowseTxt
+            // 
+            this.inFileBrowseTxt.Location = new System.Drawing.Point(67, 19);
+            this.inFileBrowseTxt.Name = "inFileBrowseTxt";
+            this.inFileBrowseTxt.Size = new System.Drawing.Size(251, 20);
+            this.inFileBrowseTxt.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Output file";
+            // 
+            // outFileBrowseBtn
+            // 
+            this.outFileBrowseBtn.Location = new System.Drawing.Point(324, 49);
+            this.outFileBrowseBtn.Name = "outFileBrowseBtn";
+            this.outFileBrowseBtn.Size = new System.Drawing.Size(75, 21);
+            this.outFileBrowseBtn.TabIndex = 7;
+            this.outFileBrowseBtn.Text = "Browse...";
+            this.outFileBrowseBtn.UseVisualStyleBackColor = true;
+            this.outFileBrowseBtn.Click += new System.EventHandler(this.OutFileBrowseBtn_Click);
+            // 
+            // outFileBrowseTxt
+            // 
+            this.outFileBrowseTxt.Location = new System.Drawing.Point(67, 50);
+            this.outFileBrowseTxt.Name = "outFileBrowseTxt";
+            this.outFileBrowseTxt.Size = new System.Drawing.Size(251, 20);
+            this.outFileBrowseTxt.TabIndex = 6;
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(227, 100);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(74, 13);
+            this.statusLabel.TabIndex = 6;
+            this.statusLabel.Text = "Status: Ready";
+            // 
             // MainView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(429, 186);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.hashBtn);
-            this.Controls.Add(this.outputTxt);
-            this.Controls.Add(this.inputTxt);
             this.Name = "MainView";
-            this.Text = "Form1";
+            this.Text = "SHAsher";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.threadCounter)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox inputTxt;
-        private System.Windows.Forms.TextBox outputTxt;
         private System.Windows.Forms.Button hashBtn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.NumericUpDown threadCounter;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button inFileBrowseBtn;
+        private System.Windows.Forms.TextBox inFileBrowseTxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button outFileBrowseBtn;
+        private System.Windows.Forms.TextBox outFileBrowseTxt;
+        private System.Windows.Forms.ComboBox dllChooseCB;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label statusLabel;
     }
 }
 
--- SHAsher/SHAsher/MainView.cs
+++ SHAsher/SHAsher/MainView.cs
@@ -5,17 +5,28 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.IO;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
 
 namespace SHAsher
 {
     public partial class MainView : Form
     {
         private List<byte> bytes = new List<byte>();
+        private List<List<byte>> listOfBytes = new List<List<byte>>();
+        private List<int> N = new List<int>();
+        private List<byte[]> output = new List<byte[]>();
+        private static Semaphore semaphore = new Semaphore(1, 1);
         long l = 0;
-        int N = 0;
+        String title = "ERROR!";
+        MessageBoxButtons btn = MessageBoxButtons.OK;
+        MessageBoxIcon icon = MessageBoxIcon.Error;
+        int inputLen = 0;
+        Stopwatch stopWatch = new Stopwatch();
 
         public MainView()
         {
@@ -61,39 +72,234 @@ namespace SHAsher
                 bytes.Add((byte)(l >> (64 - i * 8)));
 
             // Count on how many 512-bit blocks input will split
-            N = bytes.Count / 64;
+            N.Add(bytes.Count / 64);
         }
 
         private void hashBtn_Click(object sender, EventArgs e)
         {
-            bytes.Clear();
-            N = 0;
-            l = 0;
+            // Preparing variables
+            List<String> input = new List<string>();
+            StreamReader inFile = null;
+            StreamWriter outFile = null;
+            bool doRest = true;
+            String message;
 
-            convertToBytes(inputTxt.Text);
-            padMessage();
-            
-            // Prepare message to be send to DLL
-            byte[] bytesArr = new byte[bytes.Count];
-            for (int i=0; i<bytes.Count; i++)
+            if (dllChooseCB.Text == "")
             {
-                bytesArr[i] = bytes[i];
+                message = "You haven't chosen the DLL!";
+                _ = MessageBox.Show(message, title, btn, icon);
+                doRest = false;
             }
 
-            // Create buffer in which C++ will store result
-            byte[] hashedValue = new byte[64];
+            // Trying to open input file
+            if (doRest)
+            {
+                statusLabel.Text = "Opening files...";
 
-            // Calculate hash values
-            hash(bytesArr, N, hashedValue);
+                try
+                {
+                    inFile = new StreamReader(inFileBrowseTxt.Text);
+                }
+                catch (Exception exc)
+                {
+                    message = "Error opening input file!";
+                    _ = MessageBox.Show(message, title, btn, icon);
+                    doRest = false;
+                    statusLabel.Text = "Status: Ready";
+                }
+            }
 
-            // Read hash values
-            string hashValue = Encoding.UTF8.GetString(hashedValue);
+            // Trying to open output file
+            if(doRest)
+            {
+                try
+                {
+                    outFile = new StreamWriter(outFileBrowseTxt.Text);
+                }
+                catch (Exception exc)
+                {
+                    message = "Error opening output file!";
+                    _ = MessageBox.Show(message, title, btn, icon);
+                    doRest = false;
+                    inFile.Close();
+                    statusLabel.Text = "Status: Ready";
+                }
+            }
 
-            // Send output to text box
-            outputTxt.Text = hashValue;
+            // If files are opened correctly, then preparing and hashing
+            if (doRest)
+            {
+                statusLabel.Text = "Status: Preparing input";
+
+
+                // Reading the input
+                String line;
+                int counter = 0;
+                while ((line = inFile.ReadLine()) != null)
+                {
+                    input.Add(line);
+                    counter++;
+                }
+
+                inFile.Close();
+                N.Clear();
+
+                // Preparing each input line to be hashed
+                foreach (String inline in input)
+                {
+                    bytes.Clear();
+                    l = 0;
+
+                    convertToBytes(inline);
+                    padMessage();
+
+                    listOfBytes.Add(bytes);
+                }
+
+                inputLen = listOfBytes.Count();
+
+                // Prepare ThreadPool
+                ThreadPool.SetMinThreads((int)threadCounter.Value, 0);
+                ThreadPool.SetMaxThreads((int)threadCounter.Value, 0);
+
+                // Create/Prepare output list
+                for (int i = 0; i < inputLen; i++)
+                {
+                    output.Add(new byte[64]);
+                }
+
+                statusLabel.Text = "Status: Hashing...";
+
+                stopWatch.Restart();
+                // Start threads
+                if(dllChooseCB.Text == "C++")
+                {
+                    stopWatch.Start();
+                    doThreads(true);
+                    stopWatch.Stop();
+                }
+                else if(dllChooseCB.Text == "Assembler")
+                {
+                    stopWatch.Start();
+                    doThreads(false);
+                    stopWatch.Stop();
+                }
+
+                statusLabel.Text = "Status: Writing output...";
+
+                // Output
+
+                outFile.Close();
+
+                statusLabel.Text = "Status: Ready";
+            }
+        }
+
+        void doThreads(bool isCpp)
+        {
+            if (isCpp)
+            {
+                foreach(List<byte> arr in listOfBytes)
+                {
+                    ThreadPool.QueueUserWorkItem(new WaitCallback(threadJobCpp));
+                }
+            }
+            else
+            {
+                foreach (List<byte> arr in listOfBytes)
+                {
+                    ThreadPool.QueueUserWorkItem(new WaitCallback(threadJobASM));
+                }
+            }
+        }
+
+        private void threadJobCpp(object callback)
+        {
+            // Get your string id
+            int iteration;
+            try
+            {
+                semaphore.WaitOne();
+                iteration = inputLen;
+                inputLen--;
+            }
+            finally
+            {
+                semaphore.Release();
+            }
+            iteration--;
+
+            if (iteration > -1)
+            {
+                // Prepare message to be send to DLL
+                byte[] bytesArr = new byte[bytes.Count];
+                for (int i = 0; i < bytes.Count; i++)
+                {
+                    bytesArr[i] = listOfBytes[iteration][i];
+                }
+
+                // Create buffer in which C++ will store result
+                byte[] hashedValue = new byte[64];
+
+                // Calculate hash values
+                //hash(bytesArr, N[N.Count()], hashedValue);
+
+                // Save hash to output list
+                output[iteration] = hashedValue;
+
+                //string hashValue = Encoding.UTF8.GetString(hashedValue);
+
+                // Send output to text box
+                //outputTxt.Text = hashValue;
+            }
+        }
+
+        private void threadJobASM(object callback)
+        {
+
         }
 
         [DllImport("HighLevelHashLibrary.dll")]
         public static extern void hash(byte[] bytes, int N, byte[] buf);
+
+        private void OutFileBrowseBtn_Click(object sender, EventArgs e)
+        {
+            var fileDialog = new System.Windows.Forms.OpenFileDialog();
+            var result = fileDialog.ShowDialog();
+            switch (result)
+            {
+                case System.Windows.Forms.DialogResult.OK:
+                    var file = fileDialog.FileName;
+                    if (Path.GetExtension(file) != ".txt")
+                        _ = MessageBox.Show("Plik musi być rozszerzenia .txt", title, btn, icon);
+                    else
+                        outFileBrowseTxt.Text = file;
+                    break;
+                case System.Windows.Forms.DialogResult.Cancel:
+                default:
+                    outFileBrowseTxt.Text = null;
+                    break;
+            }
+        }
+
+        private void InFileBrowseBtn_Click(object sender, EventArgs e)
+        {
+            var fileDialog = new System.Windows.Forms.OpenFileDialog();
+            var result = fileDialog.ShowDialog();
+            switch (result)
+            {
+                case System.Windows.Forms.DialogResult.OK:
+                    var file = fileDialog.FileName;
+                    if (Path.GetExtension(file) != ".txt")
+                        _ = MessageBox.Show("Plik musi być rozszerzenia .txt", title, btn, icon);
+                    else
+                        inFileBrowseTxt.Text = file;
+                    break;
+                case System.Windows.Forms.DialogResult.Cancel:
+                default:
+                    inFileBrowseTxt.Text = null;
+                    break;
+            }
+        }
     }
 }
