f469c8c4f05fb6e7ba8ba86339d952c056bed323
Dominik Wolny <wolny.dominik@outlook.com>
2020-01-26 19:30:15 +0100

    Cleared mess in project folder

--- SHAsher/HashingLibraries/HighLvlHasher.h
+++ SHAsher/HashingLibraries/HighLvlHasher.h
@@ -6,7 +6,7 @@
 
 namespace hasher
 {
-	extern "C" _declspec(dllexport) void _cdecl hash(uint32_t* bytes, int Nblocks, char* return_buffer);
+	extern "C" _declspec(dllexport) void _cdecl hashCpp(uint32_t* bytes, int Nblocks, char* return_buffer);
 
 	uint32_t rotate_right(uint32_t input, uint32_t bit_count);
 	uint32_t Ch(uint32_t x, uint32_t y, uint32_t z);
@@ -15,8 +15,6 @@ namespace hasher
 	uint32_t big_sigma1(uint32_t x);
 	uint32_t small_sigma0(uint32_t x);
 	uint32_t small_sigma1(uint32_t x);
-	void init_hash_vals();
-	void calculate_hash();
 }
 
 #endif
